cbf3e3efc8fb336879ff1876605db8c671ed43a7
sunzhongfei <SUNZHONGFEI006@pingan.com.cn>
2018-11-22 14:35:19 +0800

    jitpack没有生成

---
+++ app/src/main/java/sunday/sdk/camerademo/MainActivity.java
@@ -0,0 +1,15 @@
+package sunday.sdk.camerademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sunday.sdk.camera.CameraManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
